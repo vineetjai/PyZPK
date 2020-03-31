d26b7870e922d352886355358760f0ab2f2b9682
Vineet Jain <vineetj869@gmail.com>
2020-03-31 14:14:21 +0530

    now libsnark can be cloned

--- build_MacOS.sh
+++ build_MacOS.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-cd libsnark
-mkdir build
-cd build
-#Sync & Initialization of libsnark
-git submodule sync
-git submodule init
-# Update libsnark in order to pull down the files.
-git submodule update --init --recursive
-# Reset all submodules
-git submodule foreach --recursive git reset --hard
-
+#installing Dependencies
 brew list cmake || brew install cmake
 brew list gmp || brew install gmp
 brew list boost || brew install boost
 brew list gcc || brew install gcc
+
+# Removing old directory
+rm -rf libsnark
+
+# Update libsnark in order to pull down the files.
+git submodule update libsnark
+
+# Reset all submodules
+git submodule foreach --recursive git reset --hard
+
+cd libsnark
+mkdir build
+cd build
 
 ln -s /usr/local/opt/openssl/include/openssl /usr/local/include
 cmake -DWITH_PROCPS=OFF -DWITH_SUPERCOP=OFF ..
